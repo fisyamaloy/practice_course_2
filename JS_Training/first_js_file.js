740ef83eca360965eeb004a3b8688d5f82f59b9e
Данил Фисюк <fisyuklol@gmail.com>
2021-06-08 17:36:19 +0300

    Not for work's delivery

---
+++ JS_Training/first_js_file.js
@@ -0,0 +1,10 @@
+//document.write("hello!");
+console.log("hi, bitch");
+console.info("I am comming later");
+//console.error("Something bad");
+console.warn("Something world be better");
+
+const a = 3;
+var   b = 10;
+console.log(a + b + " = 13");
+console.log("13 != " + a + b);
